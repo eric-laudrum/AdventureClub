b6b492d2f2e13ed5a54696de09bd762e27f49693
eric-laudrum <ericlaudrum@gmail.com>
2026-01-18 13:54:07 -0500

    Initialize backend with express

---
+++ back-end/src/server.js
@@ -0,0 +1,18 @@
+import express from 'express';
+
+const app = express();
+
+app.use(express.json());
+
+app.get('/hello', function(req, res){
+    res.send('Hello from GET endpoint');
+
+})
+
+app.post('/hello', function(req, res){
+    res.send('Hello, ' + req.body.name + ' from POST endpoint')
+})
+
+app.listen(8000, function(){
+    console.log('Server is listening on port 8000');
+})
