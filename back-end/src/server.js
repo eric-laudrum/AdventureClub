2663db164d1c934ab3fbb012d36f02ac94d3edfb
eric-laudrum <ericlaudrum@gmail.com>
2026-01-20 16:01:33 -0500

    Add vite and establish basic routes

--- back-end/src/server.js
+++ back-end/src/server.js
@@ -1,18 +1,55 @@
 import express from 'express';
 
+const articleInfo = [
+    { name: 'testing_1', upvotes: 0, comments: [] },
+    { name: 'testing_2', upvotes: 0, comments: [] },
+    { name: 'testing_3', upvotes: 0, comments: [] },
+]
+
 const app = express();
 
 app.use(express.json());
 
+// Hello GET for testing`
 app.get('/hello', function(req, res){
     res.send('Hello from GET endpoint');
 
 })
 
-app.post('/hello', function(req, res){
-    res.send('Hello, ' + req.body.name + ' from POST endpoint')
+// Upvote an article
+app.post('/api/articles/:name/upvote', (req, res)=>{
+    const article = articleInfo.find(a => a.name === req.params.name);
+    article.upvotes += 1;
+
+    res.json(article);
 })
 
+
+// Comment on an article
+app.post('/api/articles/:name/comments', (req, res)=>{
+    // const name = req.params.name;
+    const {name} = req.params;
+    const {postedBy, text } = req.body;
+    const article = articleInfo.find(a => a.name === name);
+    article.comments.push({
+        postedBy,
+        text,
+    });
+    res.json(article);
+})
+
+
+// GET hello name
+app.get('/hello/:name', function(req, res){
+    res.send('Hello ' + req.params.name);
+});
+
+// POST hello name
+app.post('/hello', function(req, res){
+    res.send('Hello, ' + req.body.name + ' from POST endpoint')
+});
+
+// Start server message
 app.listen(8000, function(){
     console.log('Server is listening on port 8000');
-})
+});
--- front-end/src/article-content.js
+++ front-end/src/article-content.js
@@ -1,19 +1,24 @@
 const articles = [
     {
-        name: 'testing',
-        title: 'Testing testing testing testing',
+        name: 'testing_1',
+        title: 'testing_1_title',
         content: [
             'Welcome to the page. This is content fo the page to display for the user, and for the user to read. Should they choose not to read the content, that\'s okay too'
         ]
     },
 
-    {
-        name: 'test2',
-        title: 'Testing 2',
+        {
+        name: 'testing_2',
+        title: 'testing_2_title',
         content: [
-            'Testing number two. The second test.'
+            'Welcome to the second page. This is content fo the page to display for the user, and for the user to read. Should they choose not to read the content, that\'s okay too'
         ]
-        
+    },        {
+        name: 'testing_3',
+        title: 'testing_3_title',
+        content: [
+            'Welcome to the third page. This is content fo the page to display for the user, and for the user to read. Should they choose not to read the content, that\'s okay too'
+        ]
     }
 ];
 
