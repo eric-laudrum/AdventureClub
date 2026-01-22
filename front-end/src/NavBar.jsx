fbf756c00e4e256eac0d0dab08bd87f86d7a8f31
eric-laudrum <ericlaudrum@gmail.com>
2026-01-22 01:03:43 -0500

    Implement firebase user account creation and sign in

--- front-end/src/NavBar.jsx
+++ front-end/src/NavBar.jsx
@@ -1,18 +1,41 @@
 import { Link } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
+import { getAuth, signOut} from 'firebase/auth';
+import useUser from "./use_user";
 
 export default function NavBar() {
+    const { isLoading, user } = useUser();
+
+    const navigate = useNavigate();
+
     return(
         <nav className='nav_bar'>
             <ul className='nav_links'>
                 <li className='nav_link'>
                     <Link to='/'>Home</Link>
-                    </li>
+                </li>
                 <li className='nav_link'>
                      <Link to='/about'>About</Link>
-                    </li>
+                </li>
                 <li className='nav_link'>
                      <Link to='/articles'>Articles</Link>
-                  </li>
+                </li>
+                { isLoading ? <li>Loading...</li> :(
+                    <>
+                    { user && (
+                    <li>
+                        Logged in as { user.email }
+                    </li>
+                    )}
+                    <li>
+                        { user 
+                        ? <button onClick={()=> signOut(getAuth())}>Sign Out</button>
+                        : <button onClick={()=> navigate('/login')}>Sign In</button> }
+                    </li>
+                    </>
+                ) }
+                
+
             </ul>
         </nav>
     )
--- front-end/src/pages/CreateAccountPage.jsx
+++ front-end/src/pages/CreateAccountPage.jsx
@@ -1,7 +1,56 @@
+import {useState} from 'react';
+import {Link , useNavigate } from 'react-router-dom';
+import {getAuth, createUserWithEmailAndPassword } from 'firebase/auth';
+
 export default function CreateAccountPage(){
-    console.log("\nCreateAccountPage.jsx: ")
+
+    const [email, setEmail ] = useState('');
+    const [password, setPassword] = useState('');
+    const [confirmPassword, setConfirmPassword] = useState('');
+    const [error, setError] = useState('');
+    const navigate = useNavigate();
+
+    async function createAccount(){
+        if(password !== confirmPassword){
+            setError('Password and confirm password do not match');
+            return;
+        }
+        try{
+            await createUserWithEmailAndPassword(getAuth(), email, password);
+            navigate('/articles');
+        } catch(e){
+            setError(e.message);
+        }
+    }
 
     return(
+        <>
         <h1>Create Account</h1>
+
+        {/* Error handling */}
+        { error && <p>{error}</p>}
+
+
+        <input 
+            placeholder="Email Address"
+            value={ email }
+            onChange={e => setEmail(e.target.value)} 
+        />
+        <input 
+            placeholder="Password"
+            type="password"
+            value={ password }
+            onChange={e => setPassword(e.target.value)} 
+        />
+        <input 
+            placeholder="Confirm Password"
+            type="password"
+            value={ confirmPassword }
+            onChange={e => setConfirmPassword(e.target.value)} 
+        />
+        <button onClick={ createAccount }>Create Account</button>
+        <Link to='/login'>Already have an account? Log in here!</Link>
+
+        </>
     );
 }
